6304b0ffdf1454192d45249cbd14026c4b44dd6b
RomanKrajewski <s0553506@htw-berlin.de>
2020-05-06 01:17:32 +0200

    implement responsive canvas that shows a circle

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,39 +1,63 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <button v-on:click="reverseMessage">Reverse Message</button>
-  </div>
+    <div class="hello">
+        <h1>{{ msg }}</h1>
+        <canvas ref="imagecanvas" v-bind:width="width" v-bind:height="height"
+                id="canvas"></canvas>
+        <button v-on:click="showCircles">show circles</button>
+    </div>
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  },
-  methods: {
-    reverseMessage: function () {
-      this.msg = this.msg.split('').reverse().join('')
+    export default {
+        name: 'HelloWorld',
+        props: {
+            msg: String
+        },
+        data: function () {
+            return {
+                height: 512,
+                width: 512,
+            };
+        },
+        computed: {
+            canvas: function () {
+                return this.$refs.imagecanvas;
+            },
+            ctx: function () {
+                return this.canvas.getContext('2d');
+            }
+        },
+        methods: {
+            showCircles: function () {
+                this.ctx.beginPath();
+                this.ctx.arc(75, 75, 50, 0, Math.PI * 2, true);
+                this.ctx.fill();
+            },
+            handleResize: function () {
+                const w = Math.min(window.innerWidth - 10, 900);
+                const h = w / 2;
+                this.width = w;
+                this.height = h;
+                this.$nextTick(() => {
+                    this.showCircles();
+                })
+            }
+        },
+        mounted: function () {
+            this.handleResize()
+            window.addEventListener('resize', this.handleResize)
+        },
+        beforeDestroy: function () {
+            window.removeEventListener('resize', this.handleResize)
+        }
     }
-  }
-}
 
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+    h3 {
+        margin: 40px 0 0;
+    }
+
 </style>
